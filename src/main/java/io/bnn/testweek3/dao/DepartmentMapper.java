7ed7842ae975eb2e1c24d7076a88cba179bd0734
baolovestudy <1980374572@qq.com>
2020-02-14 14:34:56 +0800

    two all

--- src/main/java/io/bnn/testweek3/dao/DepartmentMapper.java
+++ src/main/java/io/bnn/testweek3/dao/DepartmentMapper.java
@@ -1,7 +1,13 @@
 package io.bnn.testweek3.dao;
 
+import io.bnn.testweek3.dto.out.DepartmentNode;
 import io.bnn.testweek3.po.Department;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DepartmentMapper {
     int deleteByPrimaryKey(Integer departmentId);
 
@@ -14,4 +20,7 @@ public interface DepartmentMapper {
     int updateByPrimaryKeySelective(Department record);
 
     int updateByPrimaryKey(Department record);
+
+    //    custom
+    List<DepartmentNode> selectChildren(@Param("departmentId") Integer departmentId);
 }

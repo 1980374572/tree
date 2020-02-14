ff8a6cd370c5a9d1992110c3541ba433d1cace79
baolovestudy <1980374572@qq.com>
2020-02-14 14:35:19 +0800

    two all

--- src/main/java/io/bnn/testweek3/controller/DepartmentController.java
+++ src/main/java/io/bnn/testweek3/controller/DepartmentController.java
@@ -1,11 +1,27 @@
 package io.bnn.testweek3.controller;
 
+import io.bnn.testweek3.dto.out.DepartmentNode;
+import io.bnn.testweek3.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
 
+    @Autowired
+    private DepartmentService departmentService;
 
+    @GetMapping("/getTree")
+    public List<DepartmentNode> getTree(
+            @RequestParam(required = false,defaultValue = "0")Integer departmentId
+    ){
+        List<DepartmentNode> departmentNodeList = departmentService.selectChildren(departmentId);
+        return departmentNodeList;
+    }
 }

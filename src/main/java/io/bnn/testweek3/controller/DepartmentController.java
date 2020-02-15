41d4c2b5d58d99fc711f5f14e156b65786285d04
baolovestudy <1980374572@qq.com>
2020-02-15 14:15:59 +0800

    quanbu all ok

--- src/main/java/io/bnn/testweek3/controller/DepartmentController.java
+++ src/main/java/io/bnn/testweek3/controller/DepartmentController.java
@@ -1,12 +1,13 @@
 package io.bnn.testweek3.controller;
 
+import io.bnn.testweek3.dao.DepartmentMapper;
+import io.bnn.testweek3.dto.in.DepartmentCreateInDTO;
+import io.bnn.testweek3.dto.in.DepartmentUpdateInDTO;
 import io.bnn.testweek3.dto.out.DepartmentNode;
+import io.bnn.testweek3.po.Department;
 import io.bnn.testweek3.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    @Autowired
+    private DepartmentMapper departmentMapper;
+
     @GetMapping("/getTree")
     public List<DepartmentNode> getTree(
             @RequestParam(required = false,defaultValue = "0")Integer departmentId
@@ -25,5 +29,40 @@ public class DepartmentController {
         return departmentNodeList;
     }
 
+
+    @GetMapping("/getById")
+    public Department getById(@RequestParam Integer departmentId){
+        Department department = departmentMapper.selectByPrimaryKey(departmentId);
+        return department;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody DepartmentCreateInDTO departmentCreateInDTO){
+        Department department = new Department();
+        department.setName(departmentCreateInDTO.getName());
+        department.setParentId(departmentCreateInDTO.getParentId());
+        department.setSortOrder(departmentCreateInDTO.getSortOrder());
+        departmentMapper.insertSelective(department);
+        return department.getDepartmentId();
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody DepartmentUpdateInDTO departmentUpdateInDTO){
+        Department department = new Department();
+        department.setDepartmentId(departmentUpdateInDTO.getDepartmentId());
+        department.setName(departmentUpdateInDTO.getName());
+        department.setSortOrder(departmentUpdateInDTO.getSortOrder());
+        departmentMapper.updateByPrimaryKeySelective(department);
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody Integer departmentId) throws Exception {
+        List<DepartmentNode> departmentNodes = departmentMapper.selectChildren(departmentId);
+        int size = departmentNodes.size();
+        if (size > 0){
+            throw new Exception("size greater than 0, cannot delete");
+        }
+        departmentMapper.deleteByPrimaryKey(departmentId);
+    }
 
 }
